c0e4466edbf189bad524fb2ff7ea6b048c9e43ab
nullstep <me@nullstep.com>
2022-04-29 07:58:39 +0100

    some updates

--- index.js
+++ index.js
@@ -14,17 +14,14 @@ const port = 3000;
 // globals
 
 var current_board = [];
-var players_list = [];
+var player_list = [];
 var move_list = [];
 var tile_deck = [];
-var player_hands = [];
 
 var stack = [];
 
 var player_turn = 0;
 var board_season = 0;
-var move_count = 0
-var player_count = 2;
 var board_size = 0;
 
 // routing
@@ -35,17 +32,26 @@ app.get('/get_board', (req, res) => {
 });
 
 app.get('/new_board/:size', (req, res) => {
-	output = new_board(req.params.size);
+	output = new_board(
+		req.params.size
+	);
 	res.send(output);
 });
 
 app.get('/place_tile/:player/:x/:y/:tile', (req, res) => {
-	output = place_tile(req.params.player, req.params.x, req.params.y, req.params.tile);
+	output = place_tile(
+		Number(req.params.player) - 1,
+		Number(req.params.x) - 1,
+		Number(req.params.y) - 1,
+		req.params.tile
+	);
 	res.send(output);
 });
 
 app.get('/set_players/:num', (req, res) => {
-	output = set_number_of_players(req.params.num);
+	output = set_players(
+		req.params.num
+	);
 	res.send(output);
 });
 
@@ -53,38 +59,49 @@ app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 });
 
-// methods
-
-function check_args(args) {
-	var result = true;
-	args.forEach((arg, i) => function(x) {
-		if (x == null) {
-			result = false;
-		}
-	});
-	return result;
-}
+// api functions
 
 function new_board(size) {
 	var row;
 	current_board = [];
+	player_list = [];
+	move_list = [];
+
 	for (var y = 0; y < size; y++) {
 		row = [];
+
 		for (var x = 0; x < size; x++) {
-			row.push(' ');
+			row.push({
+				'state': null,
+				'owner': null,
+				'tile': null,
+				'effect': null
+			});
 		}
+
 		current_board.push(row);
 	}
+
 	board_size = size;
 	return ok('board set');
 }
 
-function set_number_of_players(num) {
-	if (move_count = 0) {
+function set_players(num) {
+	if (get_move_count() == 0) {
 		for (var x = 0; x < num; x++) {
-			players.push(x + 1);
+			player_list.push({
+				'id': x + 1,
+				'name': '',
+				'score': 0,
+				'deck': generate_deck(),
+				'mana': [
+					'earth': 0,
+					'fire': 0,
+					'water': 0
+				]
+			});
 		}
-		player_count = num;
+
 		return ok('number of players set');
 	}
 	else {
@@ -92,22 +109,25 @@ function set_number_of_players(num) {
 	}
 }
 
-function is_move_legal(x, y, tile) {
-	return true;
-}
-
 function place_tile(player, x, y, tile) {
 	if (player == player_turn) {
 		if (is_move_legal(x, y, tile)) {
 			move_list.push({
 				'player': player,
-				'x': (x - 1),
-				'y': (y - 1),
+				'x': x,
+				'y': y,
 				'tile': tile
 			});
-			current_board[x][y] = player.toString() + tile;
+
+			var space = current_board[x][y];
+
+			space.owner = player;
+			space.tile = tile;
+			
+			current_board[x][y] = space;
 			move_count++;
 			set_player_turn();
+
 			return ok('tile placed');
 		}
 		else {
@@ -119,19 +139,40 @@ function place_tile(player, x, y, tile) {
 	}
 }
 
+// internal functions
+
+function is_move_legal(x, y, tile) {
+	return true;
+}
+
 function set_player_turn() {
-	player_turn++
-	if (player_turn > (player_count - 1)) {
+	player_turn++;
+
+	if (player_turn > (get_player_count() - 1)) {
 		player_turn = 0;
 	}
 }
 
+function generate_deck() {
+	return ['e', 'f', 'w'];
+}
+
+function get_player_count() {
+	return player_list.length;
+}
+
+function get_move_count() {
+	return move_list.length;
+}
+
 // engine
 
-// render
-
-function generate_board() {
-	return '';
+function get_next_move() {
+	return {
+		'x': 0,
+		'y': 0,
+		'tile': 'e'
+	}
 }
 
 // helpers
